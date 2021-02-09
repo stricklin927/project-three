b0825ee15ea32b319a9b6fa6563980eb234e5788
Brian Short <brian.ralph.short@gmail.com>
2021-02-09 18:40:26 -0500

    Add registration form; console.logging now no axios yet

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Register() {
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ password2, setPassword2 ] = useState("");
+
+    function changeName(e) {
+        setName(e.target.value);
+    }
+
+    function changeEmail(e) {
+        setEmail(e.target.value);
+    }
+
+    function changePassword(e) {
+        setPassword(e.target.value);
+    }
+
+    function changePasswordTwo(e) {
+        setPassword2(e.target.value);
+    }
+
+    function onSubmit(e) {
+        e.preventDefault();
+        const newUser = {
+            name: name,
+            email: email,
+            password: password,
+            password2: password2
+        }
+        console.log(newUser);
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col s8 offset-s2">
+                    <Link to="/" className="btn-flat waves-effect">
+                        <i className="material-icons left">keyboard_backspace</i>
+                        Back to Home
+                    </Link>
+                    
+                    <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                        <h4>
+                            <b>Register</b> below
+                        </h4>
+                        <p className="gray-text text-darken-1">
+                            Already have an account?
+                            <Link to="/login">Log in</Link>
+                        </p>
+                    </div>
+                    
+                    <form noValidate onSubmit={onSubmit}>
+                        <div className="input-field col s12">
+                            <input onChange={changeName}
+                                value={name}
+                                // error={errors.name}
+                                id="name"
+                                type="text"
+                                />
+                        </div>
+                        <div className="input-field col s12">
+                <input
+                  onChange={changeEmail}
+                  value={email}
+                  //error={errors.email}
+                  id="email"
+                  type="email"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={changePassword}
+                  value={password}
+                  //error={errors.password}
+                  id="password"
+                  type="password"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={changePasswordTwo}
+                  value={password2}
+                  //error={errors.password2}
+                  id="password2"
+                  type="password"
+                />
+                <label htmlFor="password2">Confirm Password</label>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Sign up
+                </button>
+              </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
