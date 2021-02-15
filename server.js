49386428075137a4db0e86281fa7d46885311780
Brian Short <brian.ralph.short@gmail.com>
2021-02-15 09:58:39 -0500

    Update if-env

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const users = require('./routes/api/users');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 //Connect to the 
 mongoose.connect(
