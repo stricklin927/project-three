95232087b3c8cd551efc3efdb49c3aeb1443cc58
Brian Short <brian.ralph.short@gmail.com>
2021-02-15 10:49:36 -0500

    Add express.static designation for client/build folder

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ const users = require('./routes/api/users');
 //Set up bodyParser middleware with Express
 //app.use(bodyParser.urlencoded({extended: false}));
 //app.use(bodyParser.json());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
