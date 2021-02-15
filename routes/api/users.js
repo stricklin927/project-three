1004ebf5b3eeba15f76057c7135d394f6d1b109a
Brian Short <brian.ralph.short@gmail.com>
2021-02-15 10:10:06 -0500

    Add process.env option for api routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
+const keys = process.env.secretOrKey || require('../../config/keys');
 const cors = require('cors');
 
 const validateRegisterInput = require('../../validation/register');
