274abc333e26ad23e89f2cd0e2b895d7f3bd1a13
Brian Short <brian.ralph.short@gmail.com>
2021-02-20 12:03:27 -0500

    Add js and useState and useEffect to alumni page

--- client/src/Pages/Alumni.js
+++ client/src/Pages/Alumni.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Alumni() {
+    const [ donors, setDonors ] = useState([]);
+    const [ name, setName ] = useState("");
+    const [ amount, setAmount ] = useState(0);
+    const [ newDonor, setNewDonor ] = useState({});
+
+    // useEffect(() => {
+    //     axios.get('/api/people/donors').then(res => {
+    //         console.log(res);
+    //         setDonors(res);
+    //     })
+    // }, [donors]);
+
+    function changeName(e) {
+        setName(e.target.value);
+    }
+
+    function changeAmount(e) {
+        setAmount(e.target.value);
+    }
+
+    function onSubmit() {
+        setNewDonor({
+            name: name,
+            amount: amount
+        });
+        console.log(newDonor);
+        axios.post('/api/people/donors', newDonor).then(res => {
+            console.log(res);
+        }).catch(err => console.log(err));
+    }
+
     return(
+        <div>
         <h1>Alumni Page</h1>
+        {/* {donors.map(giver => {
+            return (
+                <div>
+                    <p>{giver.name}</p>
+                    <p>{giver.amount}</p>
+                </div>
+            )
+        })} */}
+        <form noValidate onSubmit={onSubmit}>
+              <div>
+                <input
+                  onChange={changeName}
+                  value={name}
+                //   error={errors.email}
+                  id="name"
+                  type="text"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div>
+                <input
+                  onChange={changeAmount}
+                  value={amount}
+                //   error={errors.email}
+                  id="amount"
+                  type="number"
+                />
+                <label htmlFor="name">Amount</label>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                >
+                  Give now!
+                </button>
+              </div>
+        </form>
+        </div>
     )
 }
 
