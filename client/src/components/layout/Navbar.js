7a9e74cbda51b58be0544506ce24e44fd6668031
Alexandra Stricklin <stricklin927@gmail.com>
2021-02-20 10:36:49 -0500

    edit Navbar and add Boostrap components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+/* import Navbar from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav'; */
+import { Navbar, Nav } from 'react-bootstrap';
 
-function Navbar() {
+import '../../App.css'
+
+function Function1() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">LOGO InfoTech University</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Prospective Students</Nav.Link>
+        <Nav.Link href="#pricing">Faculty</Nav.Link>
+        <Nav.Link href="#pricing">Alumni</Nav.Link>
+      </Nav>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <a href="#login">Mark Otto</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+    );
+  };
+
+/* function Navbar() {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
@@ -20,6 +44,6 @@ function Navbar() {
         </nav>
       </div>
     );
-  }
+  } */
 
-export default Navbar;
+export default Function1;
