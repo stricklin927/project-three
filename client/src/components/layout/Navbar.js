dfc694bfb9f1e21200873b0f417c7b4cd332c1c4
Brian Short <brian.ralph.short@gmail.com>
2021-02-21 17:49:39 -0500

    Correct login link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -34,7 +34,7 @@ function NavbarOne() {
       </Nav>
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
-          Signed in as: <a href="#login">{user}</a>
+          Signed in as: <a href="/login">{user}</a>
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
