0ee14307889147501d53345ecd959b4f4d728bdc
Imari Pickens <imaridp23@gmail.com>
2021-02-21 12:32:02 -0500

    Updating styling

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -34,42 +34,52 @@ function Login(props) {
               home
             </Link>
             <div>
-              <h4>
-                <b>Login</b> below
+              <h4 className="text-center p-4">
+                Enter your <b>Login</b> Email and Password 
               </h4>
-              <p>
+            </div>
+            <div className="container bg-secondary mx-auto" style={{ width: "50%" }}>
+              <form noValidate onSubmit={onSubmit}>
+                <div className="input-group py-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text material-icons" id="inputGroup-sizing-default"> account_box </span>
+                  </div>
+                  <input className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
+                    onChange={changeEmail}
+                    value={email}
+                  //   error={errors.email}
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="input-group pb-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text material-icons" id="inputGroup-sizing-default"> vpn_key </span>
+                  </div>
+                  <input className="form-control "
+                    onChange={changePassword}
+                    value={password}
+                  //   error={errors.password}
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  
+                </div>
+                <div className="justify-content-center pb-4">
+                  <button className="btn btn-primary btn-block"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+            <br></br>
+            <p className="text-center">
                 Don't have an account? <Link to="/register">Register</Link>
               </p>
-            </div>
-            <form noValidate onSubmit={onSubmit}>
-              <div>
-                <input
-                  onChange={changeEmail}
-                  value={email}
-                //   error={errors.email}
-                  id="email"
-                  type="email"
-                />
-                <label htmlFor="email">Email</label>
-              </div>
-              <div>
-                <input
-                  onChange={changePassword}
-                  value={password}
-                //   error={errors.password}
-                  id="password"
-                  type="password"
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
