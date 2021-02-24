8e14fa36122d11a708b32c9fd447f358be201fac
Brian Short <brian.ralph.short@gmail.com>
2021-02-23 20:59:53 -0500

    Add faculty card component to link faculty DB with colleges DB

---
+++ client/src/components/FacultyCard.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+function FacultyCard(props) {
+    const [professors, setProfessors] = useState([]);
+
+    useEffect(() => {
+        axios.get("/api/people/faculty", (res) => {
+            console.log(res);
+            setProfessors(res.data);
+        })
+    }, []);
+
+    return (
+            <div>
+            {professors.map(prof => {
+                return prof.Department === props.coll ?
+                <div>    
+                    <p>{prof.fullName}</p>
+                    <p>{prof.title}</p>
+                </div>
+            : null}
+            )}
+            </div>
+            )
+}
+
+export default FacultyCard;
