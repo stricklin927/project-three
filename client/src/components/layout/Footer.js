b7013c031a4e5974883d652def00fe5189a2f57f
Alexandra Stricklin <stricklin927@gmail.com>
2021-02-23 21:26:49 -0500

    add bootstrap and other css components to match UI theme

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -9,15 +9,16 @@ function Footer() {
         <footer>
             <Container>
                 <Row className="justify-content-lg-center">
-                    <h2>Connect with Us!</h2>
+                    <h2 className="connect">Connect with Us!</h2>
                 </Row>
+                <br></br>
                 <br></br>
                 <Row className="justify-content-lg-center">
                     <Col className="col-4">
                         <img src="/images/ITU4.png" className="pt-3 mr-5" style={{width:"250px", height:"108px"}}></img>
                     </Col>
                     <Col className="col-3 border-right">
-                        <h5 className="font-weight-bold">More</h5>
+                        <h5>More</h5>
                         <h6>About ITU</h6>
                         <h6>Prospective Students</h6>
                         <h6>Find Faculty and Staff</h6>
@@ -25,15 +26,15 @@ function Footer() {
                         <h6>Spotlight</h6>
                     </Col>
                     <Col className="col-3 border-right">
-                        <h5 className="font-weight-bold">Student Resources</h5>
+                        <h5>Student Resources</h5>
                         <h6>Majors</h6>
                         <h6>Departments</h6>
                         <h6>Meet the Staff</h6>
                     </Col>
                     <Col className="col-2">
-                        <h5 className="font-weight-bold">Phone</h5>
+                        <h5>Phone</h5>
                         <p>(555)123-4567</p>
-                        <h5 className="font-weight-bold">Address</h5>
+                        <h5>Address</h5>
                         <p>1223 Tech Way
                             <br></br>
                             Detroit, MI 45678</p>
@@ -41,9 +42,16 @@ function Footer() {
                 </Row>  
             </Container>
             <hr></hr>
+            <br></br>
+            <Container>
                 <Row className="justify-content-lg-center">
-                    <p> &copy; 2021 InfoTech University </p>
+                    <p className="eo">
+                        InfoTech University is committed to a policy of nondiscrimination and equal opportunity for all persons regardless of race, gender, color, religion, creed, national origin or ancestry, age, marital status, disability or veteran status.
+                    </p>
+                    <br></br>
+                    <p className="copyright">&copy; 2021 InfoTech University</p>
                 </Row>
+            </Container>
         </footer>
     )
 }
