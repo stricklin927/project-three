029b918b9db54e02ca53ade00e382f99e5298540
Brian Short <brian.ralph.short@gmail.com>
2021-02-24 09:13:23 -0500

    Update faculty card with updated DB model info

--- client/src/components/FacultyCard.js
+++ client/src/components/FacultyCard.js
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 function FacultyCard(props) {
-    const [professors, setProfessors] = useState([]);
+    // const [professors, setProfessors] = useState([]);
 
-    useEffect(() => {
-        axios.get("/api/people/faculty", (res) => {
-            console.log(res);
-            setProfessors(res.data);
-        })
-    }, []);
+    // useEffect(() => {
+    //     axios.get("/api/people/faculty", (res) => {
+    //         console.log(res);
+    //         setProfessors(res.data);
+    //     })
+    // }, []);
 
     return (
             <div>
-            {professors.map(prof => {
-                return prof.Department === props.coll.name ?
-                <div>    
-                    <p>{prof.fullName}</p>
-                    <p>{prof.title}</p>
-                </div>
-            : null}
-            )}
+                {props.coll.faculty.map(prof => {
+                    return (
+                    <div>    
+                        <p>{prof.fullName}</p>
+                        <p>{prof.Title}</p>
+                        <p>{prof.Department}</p>
+                    </div>
+                    )
+                })}
             </div>
             )
 }
