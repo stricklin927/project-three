4ecaa1b219a3312dedb2e71d99d04fc0e2bd2963
Brian Short <brian.ralph.short@gmail.com>
2021-02-23 20:02:15 -0500

    Add api calls and majors map component require

--- client/src/Pages/Students.js
+++ client/src/Pages/Students.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MajorsMap from "../components/MajorsMap";
 
 function Students() {
+    const [colleges, setColleges] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/people/colleges').then(res => {
+            console.log(res);
+            setColleges(res);
+        })
+    })
+
     return(
+        <div>
         <h1>Students Page</h1>
+        {colleges.map(coll => {
+            return (
+            <div>
+            <p>{coll.name}</p>
+            <MajorsMap coll={coll} />
+            </div>
+            )
+        })}
+        </div>
     )
 }
 
