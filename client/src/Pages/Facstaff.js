f81d37d8082365ad93083a98743349599053b32c
Brian Short <brian.ralph.short@gmail.com>
2021-02-25 22:08:45 -0500

    Change api call to relate faculty to colleges

--- client/src/Pages/Facstaff.js
+++ client/src/Pages/Facstaff.js
@@ -1,76 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-//import FacMap from '../components/FacMap';
+import FacMap from '../components/FacMap';
 
 function Facstaff() {
-    const [professors, setProfessors] = useState([]);
+    const [colleges, setColleges] = useState([]);
 
     useEffect(() => {
-        axios.get("/api/people/faculty").then(res => {
-            console.log(res);
-            setProfessors(res.data);
-        })
-    }, []);
+    axios.get('/api/people/colleges').then(res => {
+        //console.log(res);
+        setColleges(res.data);
+    })
+    }, [])
 
-    const alpha = () => {
-        console.log("Alpha clicked")
-        const sortedProfs = [...professors].sort(function (a, b) {
-              if (a.lastName > b.lastName) {
-                  return 1;
-              }
-              if (b.lastName > a.lastName) {
-                  return -1;
-              }
-              return 0;
-          })
-        console.log(sortedProfs);
-        setProfessors(sortedProfs);
-      }
+    // useEffect(() => {
+    //     axios.get("/api/people/faculty").then(res => {
+    //         console.log(res);
+    //         setProfessors(res.data);
+    //     })
+    // }, []);
 
-      const reversed = () => {
-        console.log("Reverse clicked")
-        const sortedProfs = [...professors].sort(function (a, b) {
-              if (a.lastName > b.lastName) {
-                  return -1;
-              }
-              if (b.lastName > a.lastName) {
-                  return 1;
-              }
-              return 0;
-          })
-        console.log(sortedProfs);
-        setProfessors(sortedProfs);
-      }
+    // const alpha = () => {
+    //     console.log("Alpha clicked")
+    //     const sortedProfs = [...professors].sort(function (a, b) {
+    //           if (a.lastName > b.lastName) {
+    //               return 1;
+    //           }
+    //           if (b.lastName > a.lastName) {
+    //               return -1;
+    //           }
+    //           return 0;
+    //       })
+    //     console.log(sortedProfs);
+    //     setProfessors(sortedProfs);
+    //   }
 
-      const byTitle = () => {
-        console.log("byTitle clicked")
-        const sortedProfs = [...professors].sort(function (a, b) {
-              if (a.Title > b.Title) {
-                  return 1;
-              }
-              if (b.Title > a.Title) {
-                  return -1;
-              }
-              return 0;
-          })
-        console.log(sortedProfs);
-        setProfessors(sortedProfs);
-      }
+    //   const reversed = () => {
+    //     console.log("Reverse clicked")
+    //     const sortedProfs = [...professors].sort(function (a, b) {
+    //           if (a.lastName > b.lastName) {
+    //               return -1;
+    //           }
+    //           if (b.lastName > a.lastName) {
+    //               return 1;
+    //           }
+    //           return 0;
+    //       })
+    //     console.log(sortedProfs);
+    //     setProfessors(sortedProfs);
+    //   }
 
-      const byDepartment = () => {
-        console.log("byDepartment clicked")
-        const sortedProfs = [...professors].sort(function (a, b) {
-              if (a.Department > b.Department) {
-                  return 1;
-              }
-              if (b.Department > a.Department) {
-                  return -1;
-              }
-              return 0;
-          })
-        console.log(sortedProfs);
-        setProfessors(sortedProfs);
-      }
+    //   const byTitle = () => {
+    //     console.log("byTitle clicked")
+    //     const sortedProfs = [...professors].sort(function (a, b) {
+    //           if (a.Title > b.Title) {
+    //               return 1;
+    //           }
+    //           if (b.Title > a.Title) {
+    //               return -1;
+    //           }
+    //           return 0;
+    //       })
+    //     console.log(sortedProfs);
+    //     setProfessors(sortedProfs);
+    //   }
+
+    //   const byDepartment = () => {
+    //     console.log("byDepartment clicked")
+    //     const sortedProfs = [...professors].sort(function (a, b) {
+    //           if (a.Department > b.Department) {
+    //               return 1;
+    //           }
+    //           if (b.Department > a.Department) {
+    //               return -1;
+    //           }
+    //           return 0;
+    //       })
+    //     console.log(sortedProfs);
+    //     setProfessors(sortedProfs);
+    //   }
 
       return(
         <div>
@@ -160,17 +167,15 @@ function Facstaff() {
             <br></br>
         
             <h1>Faculty Listing</h1>
-            <button onClick={alpha}>Alphabetical Order</button>
+            {/* <button onClick={alpha}>Alphabetical Order</button>
             <button onClick={reversed}>Reverse alpha</button>
             <button onClick={byTitle}>Sort by title</button>
-            <button onClick={byDepartment}>Sort by department</button>
-            {professors.map(prof => {
+            <button onClick={byDepartment}>Sort by department</button> */}
+            {colleges.map(coll => {
                 return (
                 <div>
-                    <div>{prof.firstName} {prof.lastName}</div>
-                    <div>{prof.Title}</div>
-                    <div>{prof.Department}</div>
-                    
+                    <div>{coll.name}</div>
+                    <FacMap coll={coll} />
                 </div>
                 )
             })}
