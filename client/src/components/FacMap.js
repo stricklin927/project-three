d86faff6a2480a4af6d2dc4de1f6d9ede3191a6b
Imari Pickens <imaridp23@gmail.com>
2021-02-26 13:21:59 -0500

    Updating format for faculty listing

--- client/src/components/FacMap.js
+++ client/src/components/FacMap.js
@@ -1,21 +1,22 @@
 import React from "react";
+import Table from 'react-bootstrap/Table'
 
 function FacMap(props) {
     return (
         <div>
             {props.coll.faculty.map(prof => {
-                return (<div>
-                            <table class="table table-striped">
-                                <tbody>
-                                    <tr>
-                                    <th scope="row"><img src={prof.Image} /></th>
-                                    <td>{prof.Title} {prof.fullName}</td>
-                                    <td>{prof.OfficeNumber} <br></br> {prof.PhoneNumber}</td>
-                                    <td>{prof.Email} </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>)
+                return (
+                        <Table striped bordered hover>
+                            <tbody>
+                                <tr>
+                                <th scope="row"><img src={prof.Image} /></th>
+                                <td>{prof.Title} {prof.fullName}</td>
+                                <td>{prof.OfficeNumber} <br></br> {prof.PhoneNumber}</td>
+                                <td>{prof.Email} </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        )
             })}
         </div>
     )
