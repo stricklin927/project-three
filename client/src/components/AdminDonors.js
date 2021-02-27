ca5b4a1afc97356cff8cbf328516a09d90e6ef7f
Brian Short <brian.ralph.short@gmail.com>
2021-02-27 11:08:12 -0500

    Add adminDonors component to delete and update donor components

---
+++ client/src/components/AdminDonors.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
+import '../alumni.css';
+
+
+function AdminDonors(props) {
+    const [ donors, setDonors ] = useState([]);
+    const [ user, setUser ] = useState({});
+
+    const history = useHistory();
+
+    useEffect(() => {
+        axios.get('/api/people/donors').then(res => {
+            console.log(res);
+            setDonors(res.data);
+        })
+    }, []);
+
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+          //console.log("User is here!");
+          const userObj = JSON.parse(localStorage.getItem("user"));
+            axios.get(`/api/users/get/${userObj.email}`).then(res => {
+              //console.log(res);
+              if (res.data.role === "Customer") {
+                  history.push('/home');
+              } else if (res.data.role === "Admin") {
+              setUser(res.data);
+              }
+            }).catch(err => console.log(err));
+        } else if (!localStorage.getItem("user")) {
+          history.push('/home');
+        }
+      }, [])
+
+      const onDelete = e => {
+          e.preventDefault();
+          console.log(e.target);
+          const id = e.target.value;
+          console.log(id);
+          //axios.delete(`/api/people/donors/:${id}`).then(res => console.log(res)).catch(err => console.log(err));
+      }
+
+    return (
+        <div>
+            {donors.map(donor => {
+                return (
+                <div class="card">
+                <div class="card-body">
+                  <p><strong>{donor.name}</strong>: <em>{donor.amount}</em></p>
+                  <div className="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" onClick={onDelete} className="btn btn-secondary">Delete</button>
+                    <button type="button" className="btn btn-secondary">Update</button>
+                  </div>
+                </div>
+              </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default AdminDonors;
