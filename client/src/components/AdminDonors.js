3d23678c1168af5435b763c1e6059a0a261aceaa
Brian Short <brian.ralph.short@gmail.com>
2021-02-27 11:13:40 -0500

    Add value to onDelete button

--- client/src/components/AdminDonors.js
+++ client/src/components/AdminDonors.js
@@ -50,7 +50,7 @@ function AdminDonors(props) {
                 <div class="card-body">
                   <p><strong>{donor.name}</strong>: <em>{donor.amount}</em></p>
                   <div className="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" onClick={onDelete} className="btn btn-secondary">Delete</button>
+                    <button type="button" onClick={onDelete} value={donor._id} className="btn btn-secondary">Delete</button>
                     <button type="button" className="btn btn-secondary">Update</button>
                   </div>
                 </div>
