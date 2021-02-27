c8afbefb0a689a41d9561cae1a95131c7b72b590
Brian Short <brian.ralph.short@gmail.com>
2021-02-27 16:31:56 -0500

    Add single donor page that uses useParams to render single donor file for updating

---
+++ client/src/components/SingleDonor.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+function SingleDonor() {
+    const [ donor, setDonor ] = useState({});
+    const [ name, setName ] = useState("");
+    const [ amount, setAmount ] = useState(0);
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`/api/people/donors/:id`).then(res => {
+            setDonor(res.data);
+        }).catch(err => console.log(err))
+    }, [])
+
+    const changeName = (e) => {
+        setName(e.target.value);
+    }
+
+    const changeAmount = e => {
+        setAmount(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      console.log(e);
+      console.log("Name: " + name);
+      console.log("Amount: " + amount);
+      const newDonor = {
+        name: name,
+        amount: parseInt(amount)
+      };
+        console.log(newDonor);
+        axios.put(`/api/people/donors/:${id}`, newDonor).then(res => {
+            console.log(res);
+        }).catch(err => console.log(err));
+    }
+
+    return (
+        <div className="m-5">
+            <form noValidate onSubmit={onSubmit}>
+            <p>ID: {id}</p>
+            <p>Name: {donor.name}</p>
+            <input onChange={changeName}
+                value={name}                     
+                id="name"
+                type="text"
+                placeholder={donor.name} />
+            <p>Amount: {donor.amount}</p>
+            <input onChange={changeAmount}
+                value={amount}
+                id="amount"
+                type="number"
+                placeholder={donor.amount}
+                />
+                <br />
+            <button className="mt-3" type="submit">Update</button>
+            </form>
+        </div>
+    )
+}
+
+export default SingleDonor;
