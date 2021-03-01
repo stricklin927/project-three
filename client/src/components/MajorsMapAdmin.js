a08472c162daade4912bbb88e47d1250604acec8
Brian Short <brian.ralph.short@gmail.com>
2021-02-28 21:47:50 -0500

    Add more spacing

--- client/src/components/MajorsMapAdmin.js
+++ client/src/components/MajorsMapAdmin.js
@@ -13,8 +13,8 @@ function MajorsMapAdmin(props) {
                     <div>
                     <li class="list-group-item">
                         {major} 
-                        <span value={props.coll._id} className="badge badge-danger">Delete</span> 
-                        <span value={props.coll._id} className="badge badge-warning">Update</span> 
+                        <span value={props.coll._id} className="badge badge-danger mx-2">Delete</span> 
+                        <span value={props.coll._id} className="badge badge-warning mx-2">Update</span> 
                     </li>
                     </div>
                 )
