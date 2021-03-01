3eb2c430350ac75de55447895d87713ded9551a0
Brian Short <brian.ralph.short@gmail.com>
2021-02-28 20:36:47 -0500

    Add admin majors component

---
+++ client/src/components/AdminMajors.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MajorsMap from "../components/MajorsMap";
+import FacultyCard from "../components/FacultyCard";
+
+function AdminMajors() {
+    const [colleges, setColleges] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/people/colleges').then(res => {
+            //console.log(res);
+            setColleges(res.data);
+        }).catch(err => console.log(err))
+    })
+
+    return (
+        <div>
+            {colleges.map(coll => {
+                return (
+                <div>
+                <h2>{coll.name}</h2>
+                <MajorsMap coll={coll} />
+                </div>
+                )
+            })}
+        </div>
+    )
+
+}
